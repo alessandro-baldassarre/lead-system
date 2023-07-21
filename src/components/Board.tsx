b38af1b6b1381663eb9d0a94fc238e768fc90728
Alessandro Baldassarre <alessandro.baldassarre.dev@gmail.com>
2023-07-21 11:26:00 +0200

    feature:complete base board draggable context

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,5 +1,27 @@
 'use client'
 
-export default function Board() {
-  return <div>Board</div>
+import { Esito, Lead } from '@/lib/types'
+import { DragDropContext, DropResult, Droppable } from 'react-beautiful-dnd'
+import Column from './Column'
+
+export default function Board({ leads }: { leads: Lead[] }) {
+  function handleDragEnd() {
+    return (result: DropResult) => {
+      console.log(result)
+    }
+  }
+
+  return (
+    <DragDropContext onDragEnd={handleDragEnd()}>
+      <Droppable droppableId="board" direction="horizontal" type="column">
+        {(provided) => (
+          <div className="flex overflow-x-auto overflow-y-hidden" {...provided.droppableProps} ref={provided.innerRef}>
+            {(Object.keys(Esito) as Array<keyof typeof Esito>).map((esito, index) => (
+              <Column key={esito} leads={leads} columnType={esito} index={index} />
+            ))}
+          </div>
+        )}
+      </Droppable>
+    </DragDropContext>
+  )
 }
