10e9ace3c234a27d748103d5aec64d126bf9fd5f
Alessandro Baldassarre <alessandro.baldassarre.dev@gmail.com>
2023-07-21 11:23:30 +0200

    feature: retrieve leads from api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,18 @@
 import Board from '@/components/Board'
 import NavBar from '@/components/NavBar'
 import Sidebar from '@/components/Sidebar'
+import { Lead } from '@/lib/types'
 
-export default function Home() {
+export default async function Home() {
+  const leads: Lead[] = await fetch('http:localhost:3000/api').then((res) => res.json())
+
   return (
     <main className="flex">
       <Sidebar />
       <div className="flex-1 flex flex-col">
         <NavBar />
         <div className="p-2 flex-1">
-          <Board />
+          <Board leads={leads} />
         </div>
       </div>
     </main>
